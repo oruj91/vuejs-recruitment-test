bc8216eb01e670d6c7258ba8fdd79f7f49cf78f6
Oruj Tatiyev <o.tatiyev@gmail.com>
2019-12-19 15:25:25 +0400

    table restructuring

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -7,8 +7,8 @@
       <v-flex xs12>
         <v-data-table
           :headers="headers"
-          :items="desserts"
-          :items-per-page="5"
+          :items="orders"
+          :items-per-page="10"
           class="elevation-1 my-8"
         ></v-data-table>
       </v-flex>
@@ -90,107 +90,108 @@
         </v-layout>
       </v-flex>-->
     </v-layout>
+    <p>{{new Date().toLocaleString('ru-RU')}}</p>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  orderID: 'HelloWorld',
 
   data: () => ({
     headers: [
       {
-        text: 'Dessert (100g serving)',
+        text: 'Order ID',
         align: 'left',
         sortable: false,
-        value: 'name',
+        value: 'orderID',
       },
-      { text: 'Calories', value: 'calories' },
-      { text: 'Fat (g)', value: 'fat' },
-      { text: 'Carbs (g)', value: 'carbs' },
-      { text: 'Protein (g)', value: 'protein' },
-      { text: 'Iron (%)', value: 'iron' },
+      { text: 'Table', value: 'tableID' },
+      { text: 'Waiter name', value: 'waiterName' },
+      { text: 'Status', value: 'orderStatus' },
+      { text: 'Order price', value: 'orderPrice' },
+      { text: 'Order time', value: 'orderTime' },
     ],
-    desserts: [
+    orders: [
       {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%',
+        orderID: '1',
+        tableID: 'a2',
+        waiterName: 'Murtuz',
+        orderStatus: 'completed',
+        orderPrice: '22 AZN',
+        orderTime: '19.12.2019, 15:19:53',
       },
       {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: '1%',
+        orderID: '2',
+        tableID: 'v3',
+        waiterName: 'Murtuz',
+        orderStatus: 'completed',
+        orderPrice: '41.3 AZN',
+        orderTime: '19.12.2019, 15:29:53',
       },
       {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: '7%',
+        orderID: '3',
+        tableID: 'm2',
+        waiterName: 'Kamran',
+        orderStatus: 'not finished',
+        orderPrice: '62.0 AZN',
+        orderTime: '19.12.2019, 16:39:00',
       },
       {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: '8%',
+        orderID: '4',
+        tableID: 'a3',
+        waiterName: 'Murtuz',
+        orderStatus: 'completed',
+        orderPrice: '14.3 AZN',
+        orderTime: '19.12.2019, 17:19:26',
       },
       {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: '16%',
+        orderID: '5',
+        tableID: 'g3',
+        waiterName: 'Kamran',
+        orderStatus: 'not finished',
+        orderPrice: '23.9 AZN',
+        orderTime: '19.12.2019, 17:35:47',
       },
       {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: '0%',
+        orderID: '6',
+        tableID: 'a4',
+        waiterName: 'Kamran',
+        orderStatus: 'completed',
+        orderPrice: '7 AZN',
+        orderTime: '19.12.2019, 19:20:25',
       },
       {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: '2%',
+        orderID: '7',
+        tableID: 'f23',
+        waiterName: 'Murtuz',
+        orderStatus: 'not finished',
+        orderPrice: '10 AZN',
+        orderTime: '19.12.2019, 19:42:21',
       },
       {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: '45%',
+        orderID: '8',
+        tableID: 'd10',
+        waiterName: 'Zaur',
+        orderStatus: 'completed',
+        orderPrice: '16.5 AZN',
+        orderTime: '19.12.2019, 20:19:53',
       },
       {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: '22%',
+        orderID: '9',
+        tableID: 'r4',
+        waiterName: 'Zaur',
+        orderStatus: 'completed',
+        orderPrice: '24.9 AZN',
+        orderTime: '19.12.2019, 20:34:11',
       },
       {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: '6%',
+        orderID: '10',
+        tableID: 'b3',
+        waiterName: 'Zaur',
+        orderStatus: 'completed',
+        orderPrice: '27 AZN',
+        orderTime: '19.12.2019, 21:19:22',
       },
     ],
 
