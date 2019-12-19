72bd33b3aef34c53d7a3c1e1352844668d9f26f0
Oruj Tatiyev <o.tatiyev@gmail.com>
2019-12-19 14:49:07 +0400

    Vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <h2>Villa Lounge</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="#"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Login</span>
+        <v-icon>mdi-account-box </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <OrderList/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OrderList from './components/OrderList';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    OrderList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
